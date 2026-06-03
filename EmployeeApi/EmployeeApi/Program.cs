655b9dfec96799783082905db7f9ef76325019f3
Dheera <dheera1199@gmail.com>
2026-06-03 18:12:14 -0500

    added webdite link in program.cs

--- EmployeeApi/EmployeeApi/Program.cs
+++ EmployeeApi/EmployeeApi/Program.cs
@@ -24,7 +24,8 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowAngular",
         policy =>
         {
-            policy.WithOrigins("http://localhost:4200")
+            policy.WithOrigins("http://localhost:4200",
+                               "https://nice-cliff-0873fad10.7.azurestaticapps.net")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
