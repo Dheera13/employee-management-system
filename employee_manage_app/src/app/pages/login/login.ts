2bbc5d554e3349e2c3d11a7431b6b8ed6cafaeed
Dheera <dheera1199@gmail.com>
2026-06-08 15:28:14 -0500

    updated login

--- employee_manage_app/src/app/pages/login/login.ts
+++ employee_manage_app/src/app/pages/login/login.ts
@@ -19,8 +19,8 @@ export class Login {
   router = inject(Router);
 
   onSubmit() {
-    // this.http.post('https://employeeapi2026-fvgxc5cxf7f9d6ep.centralus-01.azurewebsites.net/api/Auth/login', this.loginObj).subscribe({
-    this.http.post('http://localhost:5162/api/Auth/login', this.loginObj).subscribe({
+     this.http.post('https://employeeapi2026-fvgxc5cxf7f9d6ep.centralus-01.azurewebsites.net/api/Auth/login', this.loginObj).subscribe({
+    //this.http.post('http://localhost:5162/api/Auth/login', this.loginObj).subscribe({
 
       next: (result: any) => {
         console.log(result);
